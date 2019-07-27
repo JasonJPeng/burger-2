7dbcd2222fc132bbdfc2337f46c385f554fbeab9
Jason Peng <jason.j.peng@gamil.com>
2019-07-26 22:24:16 -0700

    remove files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3002;
 
 // Requiring our models for syncing
 var db = require("./models");
